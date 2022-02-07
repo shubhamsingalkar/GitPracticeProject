e2e7f7a21d76e3ba1e7eff6f70bed669d3ddcf9e
shubhamsingalkar <99030003+shubhamsingalkar@users.noreply.github.com>
2022-02-07 20:41:18 +0530

    Update Sample2.java

--- src/gitdemo/Sample2.java
+++ src/gitdemo/Sample2.java
@@ -4,7 +4,8 @@ public class Sample2 {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to sample2");
-
+		
+                System.out.println("Changes through github");
 	}
 
 }
